07017194092ef7a130d2721a5b631e02b45710a7
zlz <46663197+zlz-cn@users.noreply.github.com>
2024-06-22 13:47:24 +0800

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,16 +7,16 @@ const dockerHub = "https://registry-1.docker.io";
 
 const routes = {
   // production
-  "docker.libcuda.so": dockerHub,
-  "quay.libcuda.so": "https://quay.io",
-  "gcr.libcuda.so": "https://gcr.io",
-  "k8s-gcr.libcuda.so": "https://k8s.gcr.io",
-  "k8s.libcuda.so": "https://registry.k8s.io",
-  "ghcr.libcuda.so": "https://ghcr.io",
-  "cloudsmith.libcuda.so": "https://docker.cloudsmith.io",
+  "docker.542875667.xyz": dockerHub,
+  "quay.542875667.xyz": "https://quay.io",
+  "gcr.542875667.xyz": "https://gcr.io",
+  "k8s-gcr.542875667.xyz": "https://k8s.gcr.io",
+  "k8s.542875667.xyz": "https://registry.k8s.io",
+  "ghcr.542875667.xyz": "https://ghcr.io",
+  "cloudsmith.542875667.xyz": "https://docker.cloudsmith.io",
 
   // staging
-  "docker-staging.libcuda.so": dockerHub,
+  "docker-staging.542875667.xyz": dockerHub,
 };
 
 function routeByHosts(host) {
